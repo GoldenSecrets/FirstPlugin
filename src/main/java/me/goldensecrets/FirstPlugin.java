a577c4497903d3e02a92e254540df91336bc6fdf
Richard Simpson <magnum1997@gmail.com>
2020-08-09 15:08:48 -0700

    Add two override methods

--- src/main/java/me/goldensecrets/FirstPlugin.java
+++ src/main/java/me/goldensecrets/FirstPlugin.java
@@ -1,8 +1,19 @@
 package me.goldensecrets;
 
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirstPlugin extends JavaPlugin {
 
+	//	This is required for all plugins. This happens when your plugin is loaded
+	//	It will do things like setup config file or other basic things plugin needs.
+	// Even if you don't do anything here it is needed to load.
+	@Override
+	public void onDisable () {
+	}
+
+	// This will happen when plugin is disabled, such as shutdown or if unloaded
+	// for any other reason. It can be blank.
+	@Override
+	public void onEnable () {
+	}
 }
